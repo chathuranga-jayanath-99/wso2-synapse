f54d43891942632582478cbbd13d359ff774cdd1
Dilan Perera <r.dilanperera@gmail.com>
2025-06-04 13:26:52 +0530

    Preserve the previous logic even the client id is in msgContext

--- modules/transports/core/nhttp/src/main/java/org/apache/synapse/transport/http/conn/ClientConnFactory.java
+++ modules/transports/core/nhttp/src/main/java/org/apache/synapse/transport/http/conn/ClientConnFactory.java
@@ -96,8 +96,11 @@ public class ClientConnFactory {
             // See if there's a custom SSL profile configured for this server
             customContext = sslByHostMap.get(new RequestDescriptor(host, requestID));
             if (customContext == null) {
-                customContext = sslByHostMap.get(new RequestDescriptor(ALL_HOSTS, ""));
+                customContext = sslByHostMap.get(new RequestDescriptor(host, ""));
             }
+            if (customContext == null) {
+                customContext = sslByHostMap.get(new RequestDescriptor(ALL_HOSTS, ""));
+            }  
         }
         if (customContext != null) {
             return customContext;
